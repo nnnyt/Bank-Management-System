fa7077e3cde28b8f395634344eed65fb7cf19569
nnnyt <793313994@qq.com>
2020-05-30 22:08:24 +0800

    init all pages

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ def hello_world():
 def index():
     return render_template('index.html')
 
+@app.route('/client')
+def client():
+    return render_template('client.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/debt')
+def debt():
+    return render_template('debt.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
